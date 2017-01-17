42fa79b844eb4f5d3f0e206d577110783bd9a386
daipeipei <daipeipei@corp-ci.com>
2017-01-17 15:33:13 +0800

    login

--- application/controllers/Login.php
+++ application/controllers/Login.php
@@ -5,13 +5,15 @@
  * Date: 2017/1/16 0016
  * Time: 17:34
  */
+//放止用户直接通过路径来访问控制器，如果这样的话会显示找不到（封装）
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Login extends CI_Controller
 {
+    
     public function index()
     {
-        $this->load->view('login');
+        $this->load->view('index');
     }
 
 }
